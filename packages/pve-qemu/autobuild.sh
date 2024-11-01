daff44a5401f5418234c22fae6d5e69639577867
jiangcuo <jiangcuo@bingsin.com>
2024-11-01 14:54:43 +0800

    add pve-qemu autobuild.sh

---
+++ packages/pve-qemu/autobuild.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+PKGNAME="pve-qemu"
+
+errlog(){
+        echo $1
+        exit 1
+
+}
+
+exec_build(){
+        apt update
+        apt install libpve-access-control librados2  librados-dev -y
+        yes |mk-build-deps --install --remove
+        echo "clean "
+        make clean || echo ok
+        echo "build deb in `pwd` "
+	cd $SH_DIR/$PKGNAME/qemu
+	meson subprojects download
+	cd $SH_DIR/$PKGNAME
+        make deb
+}
+
+echo "This is $PKGNAME build scripts"
+
+
+SH_PATH=$(realpath "$0")
+SH_DIR=$(dirname $SH_PATH)
+
+cd $SH_DIR/$PKGNAME
+exec_build
