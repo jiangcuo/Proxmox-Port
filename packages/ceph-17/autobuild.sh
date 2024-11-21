116502a2bde26c28c70a1155037d8a24c6860881
jiangcuo <jiangcuo@bingsin.com>
2024-11-21 15:53:17 +0800

    fix ceph build

---
+++ packages/ceph-17/autobuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+PKGNAME=$(basename $SCRIPT_DIR)
+
+echo "This is $PKGNAME build scripts"
+
+. ../common.sh
+
+cd $SCRIPT_DIR/$PKGNAME/ceph
+apt update
+yes |mk-build-deps --install --remove
+cd $SCRIPT_DIR/$PKGNAME
+exec_build_make
