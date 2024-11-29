59a803a52e1b2000391ba355065e09c32431ae49
jiangcuo <jiangcuo@bingsin.com>
2024-11-29 14:52:01 +0800

    copy proxmox-perl-rs pkg to maindir

--- packages/proxmox-perl-rs/autobuild.sh
+++ packages/proxmox-perl-rs/autobuild.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 PKGNAME=$(basename $SCRIPT_DIR)
 
@@ -11,5 +10,5 @@ for i in common/pkg  pve-rs;
 do
 	cd $SCRIPT_DIR/$PKGNAME/$i
 	exec_build_make
+	cp *.buildinfo *.changes *.dsc *.tar* *.deb $SCRIPT_DIR/$PKGNAME/ ||true
 done
-
