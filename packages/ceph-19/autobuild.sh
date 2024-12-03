64a5b6c18f1f8ee9e5fe5ec32681035b81b7275e
jiangcuo <jiangcuo@bingsin.com>
2024-12-03 20:57:02 +0800

    update autobuil.sh for loongarch-ceph

--- packages/ceph-19/autobuild.sh
+++ packages/ceph-19/autobuild.sh
@@ -7,6 +7,13 @@ echo "This is $PKGNAME build scripts"
 . ../common.sh
 
 cd $SCRIPT_DIR/$PKGNAME/ceph
+
+arch=`arch`
+if [ "$arch" == "loongarch64" ];then
+	for i in `cat $SCRIPT_DIR/series.loongarch64.ceph`;
+		do patch -p1 < ../../$i
+	done
+fi
 apt update
 yes |mk-build-deps --install --remove
 cd $SCRIPT_DIR/$PKGNAME
