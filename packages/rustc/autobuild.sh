afb41732bd0c55a84914e523d352395c67f4a4d4
jiangcuo <jiangcuo@bingsin.com>
2024-12-08 19:46:43 +0800

    bump rustc to 1.82.0+dfsg1-1

--- packages/rustc/autobuild.sh
+++ packages/rustc/autobuild.sh
@@ -8,7 +8,9 @@ SH_PATH=$(realpath "$0")
 SH_DIR=$(dirname $SH_PATH)
 
 . ../common.sh
-export DEB_BUILD_OPTIONS="nocheck parallel=8"
+
+export DEB_BUILD_OPTIONS="nocheck"
+
 copy_dir
 exec_build_dpkg
 cp  /build/*.changes /build/*.buildinfo /build/*.deb /build/*.tar.* /build/*.dsc $SH_DIR/$PKGNAME ||true
