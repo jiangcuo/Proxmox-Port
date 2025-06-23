e97b8a450b4b986f819d43a7e55cf0ed60fb07fc
Lierfang Support Team <itsupport@lierfang.com>
2025-06-23 10:50:56 +0000

    add python3-markupsafe

---
+++ packages/markupsafe/autobuild.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+PKGNAME=$(basename $SCRIPT_DIR)
+
+echo "This is $PKGNAME build scripts"
+
+SH_PATH=$(realpath "$0")
+SH_DIR=$(dirname $SH_PATH)
+
+. ../common.sh
+
+copy_dir
+exec_build_dpkg
+cp  /build/*.changes /build/*.buildinfo /build/*.deb /build/*.tar.* /build/*.dsc $SH_DIR/$PKGNAME ||true
